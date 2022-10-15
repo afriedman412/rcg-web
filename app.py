9c1684bc676ae2a3344238d64302162573313e04
Andy Friedman <afriedman412@gmail.com>
2022-10-14 20:11:08 -0400

    layout work

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, dash_table
 import dash_bootstrap_components as dbc
-from rcg_web.code.helpers import load_chart, load_plot
+from rcg_web.code.helpers import load_chart, parse_chart, load_plot, format_features, gender_col_formatter
 
+### TODO: move formatting to another doc
 app = Dash(
     __name__,
     external_stylesheets=[dbc.themes.SLATE]
@@ -9,22 +10,87 @@ app = Dash(
 server = app.server
 
 full_chart, chart_date = load_chart()
+chart_w_features = format_features(full_chart)
+total_chart_dict = parse_chart(full_chart)
 total_fig, pct_fig = (load_plot(full_chart, False), load_plot(full_chart, True))
-total_plot, pct_plot = (dcc.Graph(figure=total_fig), dcc.Graph(figure=pct_fig))
 
-app.layout = html.Div(
+gcf = []
+for g in ['Male', 'Female', 'Non-Binary']:
+    gcf += gender_col_formatter(g, full_chart) 
+print(gcf)
+
+dt = dash_table.DataTable(
+    chart_w_features.to_dict('records'),
+    [{"name": i, "id": i} for i in chart_w_features.columns]
+)
+
+count = [
+    html.H4(
+        f"{d['gender']}: {d['count']} Appearances ({d['pct']}%)", style={"margin":"0px"})
+        for d in total_chart_dict
+    ]
+
+app.layout = dbc.Container(
     [
-        html.Div([
-            html.H2("Rap Caviar Gender Distribution", style={"margin":"0px"}),
-            html.H3(f"Week of {chart_date}", style={"margin":"0px"})],
-            style={'margin-bottom':"10px"}
+        dbc.Row(
+            dbc.Col(
+                html.H1("Rap Caviar Gender Watch", 
+                # style={"margin-bottom":"2rem"}
+                ),
+                width={"size":True}
+            )
         ),
-        html.Div(children=[
-            dcc.Graph(id='total', figure=total_fig, className="six columns"),
-            dcc.Graph(id='pct', figure=pct_fig, className="six columns"),
-        ])
-    ], 
-    style={"margin": "2rem 5rem"}
+        # date
+        dbc.Row(
+            dbc.Col(
+                html.H2(f"{chart_date}", style={"margin-top":"0rem", "margin-bottom":"2rem"}), 
+                width={"size":True})
+        ),
+        # full count
+        dbc.Row(
+            children=[
+                html.A(id="top"),
+                dbc.Col(
+                    html.Div(
+                        count,
+                        style={'margin-bottom':"2rem"}
+                    ), width={"size":True})
+            ]
+        ),
+        # bar chart
+        dbc.Row(children=[
+            html.A(id="plot"),
+            dbc.Col(
+                html.Div(children=[
+                    dcc.Graph(
+                        id=id_, 
+                        figure=fig, 
+                        className="six columns", 
+                        config={'displayModeBar': False}) 
+                        for id_, fig in [('total', total_fig), ('pct', pct_fig)]
+                        ]
+                    ),
+            style={'margin':"2rem 0rem"},
+            width={"size":True}
+            ),
+            ]),
+        # full chart
+        # dbc.Row(dt, style={'margin-top':"2rem"}),
+
+        # # gender counts
+        # dbc.Row(children=[
+        #     dbc.Col(html.H5("YYY"), width=3),
+        #     dbc.Col(html.H5("6"), width=1)
+        #     ]
+        # ),
+        # dbc.Row(children=[
+        #     dbc.Col(html.H5("XXXXX XXXXXXXX XXXXXXX XXXXXXX"), width=3),
+        #     dbc.Col(html.H5("633"), width=1)
+        #     ]
+        # ),
+
+    ],
+    style={'margin-top':"2rem"}
     )
 
 if __name__ == "__main__":
--- rcg_web/code/helpers.py
+++ rcg_web/code/helpers.py
@@ -1,6 +1,8 @@
 import datetime as dt
 import pandas as pd
 import plotly.graph_objects as go
+from dash import html
+import dash_bootstrap_components as dbc
 from .. import engine
 
 def load_chart():
@@ -20,6 +22,34 @@ def load_chart():
     chart_date = dt.datetime.strptime(chart_date, "%Y-%m-%d").strftime("%B %d, %Y")
     return full_chart, chart_date
 
+def format_features(full_chart):
+    features = full_chart.query("primary_artist_name != artist_name").groupby("song_name")['artist_name'].apply(lambda a: ", ".join(a))
+    main_chart = full_chart.query("primary_artist_name == artist_name").set_index("song_name")
+    chart_w_features = main_chart.drop(['chart_date', 'artist_name', 'gender'],1).join(features).reset_index().rename(columns={'artist_name':'features'})
+    return chart_w_features
+
+def parse_chart(full_chart):
+    total_df = full_chart['gender'].value_counts().rename_axis('gender').reset_index(name='count')
+    pct_df = full_chart['gender'].value_counts(normalize=True).rename_axis('gender').reset_index(name='pct')
+    pct_df['pct'] = pct_df['pct'].map(lambda c: c*100).round(2)
+    total_df=total_df.set_index('gender').join(pct_df.set_index("gender")).reset_index()
+    total_chart_dict = total_df.to_dict("records")
+    for k in {'Male', 'Female', 'Non-Binary'}.difference(set([d['gender'] for d in total_chart_dict])):
+        total_chart_dict.append({"gender":k, "count":0, "pct":0})
+    return total_chart_dict
+
+def gender_col_formatter(g, full_chart):
+    l = len(full_chart.query(f"gender=='{g}'"))
+    g_list = full_chart.query(f"gender=='{g}'")['artist_name'].value_counts().reset_index().values
+    artists = [a[0] for a in g_list]
+    counts = [a[1] for a in g_list]
+    gender_col = [
+        dbc.Col(children=[html.H4(g)] + [html.H6(a) for a in artists], width=3),
+        dbc.Col(children=[html.H4(l)] + [html.H6(c) for c in counts], width=1),
+        ]
+    return gender_col
+
+
 def load_plot(full_chart, normalize=False):
     count_df = full_chart['gender'].value_counts(normalize=normalize).rename_axis('gender').reset_index(name='count')
     count_df['format'] = 'Percentage' if normalize else 'Total'
@@ -30,11 +60,11 @@ def load_plot(full_chart, normalize=False):
     fig = go.Figure(
         go.Bar(
             x=count_df['gender'], y=count_df['count'],
-            hovertemplate="<b>%{x}</b>: <br>%{y:.1f}%" if normalize else "<b>%{x}</b>: <br>%{y}",
             text=count_df['count'],
             textposition='outside'
         )
     )
+
     fig.update_layout(
         title = {
             'text':"% of Total Artist Appearances" if normalize else "Total Artist Appearances",
